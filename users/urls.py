2b0fd2067ed900fed33bb00272d2ab9941313c2d
vbroosing <sanmartin.michels@gmail.com>
2026-01-13 01:27:18 -0300

    desacoplando css y js

--- users/urls.py
+++ users/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path('signin/', views.signin, name='signin'),
     path('signout/', views.signout, name='signout'),
 
-    path('dashboard/', views.dashboard, name='dashboard')
 
 ]
