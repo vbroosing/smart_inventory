02213006f477f7b45e003ce56cb0c6ed948572bb
vbroosing <sanmartin.michels@gmail.com>
2026-01-14 20:44:18 -0300

    vaciando js para refactor template dashboard_base.js

--- static/js/dashboard_base.js
+++ static/js/dashboard_base.js
@@ -1,10 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
-                var elems = document.querySelectorAll('.sidenav');
-                var instances = M.Sidenav.init(elems);
+// document.addEventListener('DOMContentLoaded', function() {
+//                 var elems = document.querySelectorAll('.sidenav');
+//                 var instances = M.Sidenav.init(elems);
                 
-                var elemsCollapsible = document.querySelectorAll('.collapsible');
-                var instancesCollapsible = M.Collapsible.init(elemsCollapsible);
+//                 var elemsCollapsible = document.querySelectorAll('.collapsible');
+//                 var instancesCollapsible = M.Collapsible.init(elemsCollapsible);
 
-                var elemsSelect = document.querySelectorAll('select');
-                var instancesSelect = M.FormSelect.init(elemsSelect);
-            });
+//                 var elemsSelect = document.querySelectorAll('select');
+//                 var instancesSelect = M.FormSelect.init(elemsSelect);
+//             });
--- users/static/users/js/signin.js
+++ users/static/users/js/signin.js
@@ -1,3 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-        M.updateTextFields();
-    });
+// document.addEventListener('DOMContentLoaded', function() {
+//         M.updateTextFields();
+//     });
